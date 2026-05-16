10d78d5f6a161f3bd23b036d197e0c524e8b5175
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-05-16 23:51:40 +0600

    basic variable declaration

---
+++ BasicProgram/var.c
@@ -0,0 +1,12 @@
+// basic variable declaration and initialization
+#include <stdio.h>
+int main()
+{
+    int num1 = 20;
+    int num2 = 30;
+    float num3 = 2.5;
+    double num4 = 2.55;
+    int sum = num1 + num2;
+    printf("%d", sum);
+    return 0;
+}
