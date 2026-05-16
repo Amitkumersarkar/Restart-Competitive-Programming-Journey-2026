1ed3ff2733a50d17465f8a20b2587e49037a38d3
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-05-17 01:46:06 +0600

    updated

--- BasicProgram/var.c
+++ BasicProgram/var.c
@@ -6,7 +6,12 @@ int main()
     int num2 = 30;
     float num3 = 2.5;
     double num4 = 2.55;
+    char name = 'a';
+
     int sum = num1 + num2;
-    printf("%d", sum);
+    printf("%d \n", sum);
+    printf("%.1f\n", num3);
+    printf("%lf\n", num4);
+    printf("%c", name);
     return 0;
 }
