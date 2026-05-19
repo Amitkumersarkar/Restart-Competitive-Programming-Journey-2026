89d2b4027a7058ea9b5408d68a714f4849e5683d
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-05-19 23:47:57 +0600

    basic user input

--- BasicProgram/userInput.c
+++ BasicProgram/userInput.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main()
 {
-    int num1, num2, sum;
+    int num1, num2;
     scanf("%d %d", &num1, &num2);
-    sum = num1 + num2;
-    printf("%d", sum);
+    int multi = num1 * num2;
+    printf(" %d", multi);
 
     return 0;
 }
