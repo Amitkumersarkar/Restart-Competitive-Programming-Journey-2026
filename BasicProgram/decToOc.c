dc74e3cab00034d134b6528d6201fb9f9db79b5c
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-05-27 02:01:19 +0600

    number conversion

---
+++ BasicProgram/decToOc.c
@@ -0,0 +1,11 @@
+// decimal to octal conversion
+// %d for dec and %o for oct
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter any decimal number : ");
+    scanf("%d", &num);
+    printf("The octal number is : %o", num);
+    return 0;
+}
