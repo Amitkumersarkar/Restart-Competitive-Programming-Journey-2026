be8200c2e74050807111e3eae9bb3e8f45939870
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-05-27 16:40:26 +0600

    avg value

--- BasicProgram/arithmatic01.c
+++ BasicProgram/arithmatic01.c
@@ -5,6 +5,9 @@ int main()
     printf("Enter two number : ");
     scanf("%d %d", &num1, &num2);
     sum = num1 + num2;
-    printf("The sum is : %d", sum);
+    printf("The sum is : %d\n", sum);
+
+    float avg = (float)sum / 2;
+    printf("The average value is : %.1f\n", avg);
     return 0;
 }
