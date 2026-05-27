626cecdcf599ce9f82ab634130911df533f8aaaf
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-05-27 15:43:05 +0600

    hexa to decimal

---
+++ BasicProgram/hexTodec.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int num;
+    printf("Enter your hexaDecimal Number : ");
+    scanf("%x", &num);
+    printf("The decimal number : %d", num);
+    return 0;
+}
