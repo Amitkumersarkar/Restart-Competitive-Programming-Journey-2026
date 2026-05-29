c14ed7839ff9640bb6351027c9028752335a211c
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-05-30 02:16:17 +0600

    updated file

--- ClearDay.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int X, Y;
-    scanf("%d %d", &X, &Y);
-    int clear = 7 - X - Y;
-    printf("%d\n", clear);
-    return 0;
-}
--- DoubleRent.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-int main()
-{
-    int X;
-    scanf("%d", &X);
-    printf("%d\n", X * 2);
-    return 0;
-}
