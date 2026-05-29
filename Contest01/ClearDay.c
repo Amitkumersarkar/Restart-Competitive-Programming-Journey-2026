c0641ed0ddfdfe1e35d50e5ac343e6ab1cef2dce
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-05-30 02:14:49 +0600

    updated file structure

---
+++ Contest01/ClearDay.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+    int clear = 7 - X - Y;
+    printf("%d\n", clear);
+    return 0;
+}
---
+++ Contest01/Moneymaking.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+    printf("%d", 5000 * X + 9800 * Y);
+    return 0;
+}
