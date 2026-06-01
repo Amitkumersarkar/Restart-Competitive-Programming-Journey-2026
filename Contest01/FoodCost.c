498a6256d69fe4f5f5ed9c5c022670a179a5ef94
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-01 19:47:17 +0600

    food cost prob solve

---
+++ Contest01/FoodCost.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int X, Y;
+    scanf("%d %d", &X, &Y);
+    int cal = X * 6 + Y;
+    printf("%d\n", cal);
+    return 0;
+}
---
+++ Contest01/tutionsFees.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    int N;
+    scanf("%d", &N);
+    int t = N * N;
+    printf("%d\n", t);
+    return 0;
+}
