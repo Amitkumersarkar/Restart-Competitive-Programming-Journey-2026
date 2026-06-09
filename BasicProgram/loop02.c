4fe835b2d2bdc36a92dc80f65afa0e0013d4c4ee
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-09 23:13:51 +0600

    basic while loop

---
+++ BasicProgram/loop02.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    int i = 1; // variable declarations and initializations
+    while (i <= 10)
+    {
+        printf("I Will Do It\n");
+        i++;
+    }
+    return 0;
+}
