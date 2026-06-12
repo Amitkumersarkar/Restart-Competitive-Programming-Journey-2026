05e31e30548052f0700ddb9946225ca9cba48246
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-13 00:53:39 +0600

    candy division prob solve

---
+++ Contest03/candyDivision.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++)
+    {
+        int n;
+        scanf("%d", &n);
+        if (n % 3 == 0)
+        {
+            printf("YES\n");
+        }
+        else
+        {
+            printf("NO\n");
+        }
+    }
+    return 0;
+}
