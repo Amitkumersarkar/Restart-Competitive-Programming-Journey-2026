269dab746effe0322153fbb9bc73470fb18563c0
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-12 13:35:41 +0600

    basic loop practice task

---
+++ BasicProgram/pracTask03.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int i = 1; // initialization
+    do
+    {
+        printf("%d\n", i);
+        i++; // update
+    } while (i <= 5); // conditions
+
+    return 0;
+}
