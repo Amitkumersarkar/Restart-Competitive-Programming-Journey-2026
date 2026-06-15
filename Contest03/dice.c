b112932826b0511dd2cc394e50a61ccd77953f51
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-16 00:32:33 +0600

    unsolve dice prob

---
+++ Contest03/dice.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int t;
+    scanf("%d", &t);
+    for (int i = 1; i <= t; i++)
+    {
+        int a1, a2, a3, b1, b2, b3;
+        scanf("%d %d %d %d %d %d", &a1, &a2, &a3, &b1, &b2, &b3);
+    }
+    return 0;
+}
