e4a43aa396b042b9941ee86411efce4c6288abaf
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-19 01:15:16 +0600

    updated code

--- BasicProgram/continueInloop.c
+++ BasicProgram/continueInloop.c
@@ -3,7 +3,7 @@ int main()
 {
     for (int i = 0; i <= 6; i++)
     {
-        if (i % 2 == 0)
+        if (i % 2 == 0) // if value of i is even then continue
         {
             continue;
         }
