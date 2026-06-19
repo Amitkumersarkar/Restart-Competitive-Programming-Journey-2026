38dac6484324174fddfed523b14b6b31e1e431ed
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-20 01:16:46 +0600

    basic break

---
+++ BasicProgram/basicArray01.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int t = 10000000;
+    int arr[10000000];
+    // scanf("%d %d", &t, &arr[t]);
+    for (int i = 0; i <= t; ++i)
+    {
+        // printf("%d\n", arr[]);
+        arr[i] = i;
+    }
+    return 0;
+}
