4fc426706995a1ab5bfa32e5769912e7749c36bc
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-23 23:02:06 +0600

    fixes bugs

--- BasicProgram/prime01.c
+++ BasicProgram/prime01.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
+
 int main()
 {
     int n, count = 0;
     scanf("%d", &n);
+
+    if (n < 2)
+    {
+        printf("Not a prime number\n");
+        return 0;
+    }
+
     for (int i = 2; i < n; i++)
     {
         if (n % i == 0)
@@ -11,13 +19,11 @@ int main()
             break;
         }
     }
+
     if (count == 0)
-    {
         printf("Prime number\n");
-    }
     else
-    {
         printf("Not a prime number\n");
-    }
+
     return 0;
 }
