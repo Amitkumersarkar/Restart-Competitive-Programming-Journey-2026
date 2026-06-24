f70b9397b1f055e326cf24ddac107ad410c8893b
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-24 15:16:58 +0600

    fixes error on gcd calculation

--- BasicProgram/gcd.c
+++ BasicProgram/gcd.c
@@ -3,9 +3,13 @@ int main()
 {
     int n1, n2, rem;
     scanf("%d %d", &n1, &n2);
-    rem = n1 % n2;
-    n1 = n2;
-    n2 = rem;
-    printf("%d\n", n2);
+    while (n2 != 0)
+    {
+        rem = n1 % n2;
+        n1 = n2;
+        n2 = rem;
+    }
+
+    printf("%d\n", n1);
     return 0;
 }
