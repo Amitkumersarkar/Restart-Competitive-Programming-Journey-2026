4c4680255e2568f61d561340900d0df1bdc96d18
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-26 23:54:13 +0600

    digit extraction done

--- BasicProgram/LoopPracticeProb/digitExtraction.c
+++ BasicProgram/LoopPracticeProb/digitExtraction.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int x, n;
-    scanf("%d", &n);
+    int x;
+    scanf("%d", &x);
+    while (x != 0)
+    {
+        int lastDigit = x % 10;
+        printf("%d\n", lastDigit);
+        x /= 10;
+    }
+
     return 0;
 }
