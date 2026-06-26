b4125162de1648e5d7a58ac00b733025f60fe750
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-26 23:59:25 +0600

    updated

--- BasicProgram/LoopPracticeProb/digitExtraction.c
+++ BasicProgram/LoopPracticeProb/digitExtraction.c
@@ -5,9 +5,11 @@ int main()
     scanf("%d", &x);
     while (x != 0)
     {
+        printf("Current = %d\n", x);
         int lastDigit = x % 10;
-        printf("%d\n", lastDigit);
+        printf("Extracted = %d\n", lastDigit);
         x /= 10;
+        printf("New = %d\n\n", x);
     }
 
     return 0;
