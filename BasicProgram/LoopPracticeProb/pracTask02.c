4b5af74d9e335bc4495828bc74a07b7dcf5772b9
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-06-27 12:22:52 +0600

    another task done

---
+++ BasicProgram/LoopPracticeProb/pracTask02.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main()
+{
+    int x, sum = 0;
+    scanf("%d", &x);
+    while (x != 0)
+    {
+        int last = x % 10;
+        sum = sum + last;
+        x /= 10;
+    }
+    printf("%d\n", sum);
+    return 0;
+}
