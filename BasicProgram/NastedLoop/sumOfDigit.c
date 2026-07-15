5ad5bb67b5dc56e92a48759cf86cf1431d370195
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-15 12:08:27 +0600

    digital root using while loop

---
+++ BasicProgram/NastedLoop/sumOfDigit.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int main()
+{
+    int n;
+    scanf("%d", &n);
+    while (n > 9)
+    {
+        int x = n;
+        int sum = 0;
+        while (x != 0)
+        {
+            int last = x % 10;
+            sum += last;
+            x /= 10;
+        }
+        n = sum;
+    }
+    printf("%d\n", n);
+    return 0;
+}
