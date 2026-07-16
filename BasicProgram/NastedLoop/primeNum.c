1c8b4820e6088b4f4e1e481dd618a880fbb9b9ff
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-16 19:38:28 +0600

    prime number

---
+++ BasicProgram/NastedLoop/primeNum.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+int main()
+{
+    int n = 100;
+    // scanf("%d", &n);
+    for (int i = 2; i <= n; i++)
+    {
+        int count = 0;
+        for (int j = 2; j < i; j++)
+        {
+            if (i % j == 0)
+            {
+                count++;
+            }
+        }
+        if (count == 0)
+        {
+            printf("%d\n", i);
+        }
+    }
+    return 0;
+}
