e9e367c930b309f9878f525660d6f01329097664
Amitkumersarkar <amitekumersarkar@gmail.com>
2026-07-19 10:04:20 +0600

    sum in series

--- BasicProgram/series/series01.c
+++ BasicProgram/series/series01.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 int main()
 {
-    int n;
+    int n, sum = 0;
     // printf("Enter the last number of the series : ");
     scanf("%d", &n);
-    printf("1+2+3+....+%d", n);
+    printf("1+2+3+....+ %d", n);
+    for (int i = 1; i <= n; i = i + 1)
+    {
+        sum = sum + i;
+    }
+    printf(" = %d\n", sum);
     return 0;
 }
